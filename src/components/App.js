44c28400b7501c7a0113c360d29eb792f2a8dc8c
Sajeed Shaikh <sajeed.shaikh@neebal.com>
2021-12-24 12:09:41 +0530

    Remove Useless Stuff
    * Remove then contents
    * Added toggle in Header

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Header from './Bootstrap/Header';
+
+class App extends Component {
+  state = {
+    Dark: true
+  }
+
+  toggleHeader = () => {
+    this.setState({
+      Dark: !this.state.Dark
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header dark={this.state.Dark} className="mb-5 justify-content-center">
+          Scalar Portfolio Generator
+          <button 
+            className="btn btn-sm btn-outline-primary ml-3 rounded-circle" 
+            onClick={this.toggleHeader}
+          >
+            <i className={"fa fa-" + (this.state.Dark ? "sun" : "moon") + "-o"}></i>
+          </button>
+        </Header>
+      </div>
+    )
+  }
+}
+
+export default App;
